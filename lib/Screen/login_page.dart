35ba4761d9264ad831117ce5118821d22264dfe3
prikshitmaster <prikshitcorp@gmail.com>
2022-07-07 20:11:09 +0530

    form validation is completed

--- lib/Screen/login_page.dart
+++ lib/Screen/login_page.dart
@@ -10,17 +10,30 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+
+ final _formKey = GlobalKey<FormState>();
+   moveTohome(BuildContext context)async{
+
+       if(_formKey.currentState!.validate()){
+         Navigator.pushNamed(context, MyRoute.homeRoute);
+         setState(() {
+
+         });
+       }
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text(" login page"),
+        title:  const Text(" login page"),
       ),
       body: SingleChildScrollView(
         child: Column(
           children:  [
-            const Padding(
+             const Padding(
                 padding: EdgeInsets.zero,
                 child: Image(
                   image: AssetImage('assets/loginimage.png'),
@@ -28,52 +41,73 @@ class _LoginPageState extends State<LoginPage> {
                   height: 270,
                   width: 500,
                 )),
-            const SizedBox(
+             const SizedBox(
               height: 20,
             ),
              Text(
               "Welcome $name",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style:  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
 
-           const SizedBox(
+            const SizedBox(
              height: 40,
            ),
                 Padding(
-                  padding:  EdgeInsets.symmetric(vertical: 20 , horizontal: 30),
-                  child: Column(
-                    children:  [
-                      TextField(
-                        decoration: InputDecoration(
-                          hintText: "Enter Username ",
-                          labelText: "Username"
+                  key: _formKey,
+                  padding:   const EdgeInsets.symmetric(vertical: 20 , horizontal: 30),
+                  child: Form (
+
+                    key: _formKey,
+                    child: Column(
+                      children:  [
+                        TextFormField(
+
+
+                          decoration:   InputDecoration(
+                            hintText: "Enter Username ",
+                            labelText: "Username",
+                          ),
+
+                             validator: ( value){
+                            if(value!.isEmpty){
+                              return "enter your username";
+                            }else{
+                              return null;
+                            }
+                             },
+                        ),      TextFormField(
+                        obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: "Enter Password ",
+                            labelText: "Password",
+
+                          ),
+                                 validator: (value){
+                                    if(value!.length <6){
+                                      return " Aleast should be 6 character";
+                                    }
+                                    else if(value!.isEmpty
+                                    ){
+                                      return "Enter your password";
+                                    }
+                                    else{
+                                      return null;
+                                    }
+                                 },
+
+
+
                         ),
-                        onChanged:(value){
-                          name = value;
-                          setState(() {
+                       const SizedBox(
+                         height: 20,
+                       ),
+                         ElevatedButton(
+                             child: const Text('Login'),
+                           onPressed: () => moveTohome(context),
 
-                          });
-                        },
-
-                      ),    TextField(
-                      obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: "Enter Password ",
-                          labelText: "Password",
-
-                        ),
-
-                      ),
-                     SizedBox(
-                       height: 20,
-                     ),
-                       ElevatedButton(
-                           child: Text('Login'),
-                         onPressed: (){
-                           Navigator.pushNamed(context, MyRoute.homeRoute);
-                         },
-                        ),
-                    ],
+                          ),
+                      ],
+                    ),
                   ),
                 )
 
